30f4aa10d33f4820259b0301a72c27eee0c86730
Timothy Anthony <anthony.timothy90@gmail.com>
2018-09-13 23:24:27 +0200

    feat(MitigacionRiesgo) Optimizing ExecuteFunctions()

--- MitigacionRiesgo.js
+++ MitigacionRiesgo.js
@@ -9,8 +9,18 @@
 
 var catsg = 0;
 var catrsg = 0;
-var catsg_wsid = 0;
-var catrsg_wsid = 0;
+var catsg_wsid = "";
+var catrsg_wsid = "";
+
+var cbws = new cbWSClient('');
+cbws.extendSession();
+
+ExecuteFunctions('getModuleWebseriviceID','wsmodule=CatalogoSalvaGuarda').then(function(data) {
+    catsg_wsid = data+'x';
+});
+ExecuteFunctions('getModuleWebseriviceID','wsmodule=CatalogoRiesgos').then(function(data) {
+    catrsg_wsid = data+'x';
+});
 
 function MitigacionRiesgosetValueFromCapture(recordid, value, target_fieldname) {
     if (target_fieldname=='catsg') {
@@ -22,33 +32,22 @@ function MitigacionRiesgosetValueFromCapture(recordid, value, target_fieldname) 
     }
 
     if(catsg !=0 && catrsg !=0) {
-        var cbws = new cbWSClient('');
-        cbws.extendSession();
+        cbws.doQuery("select redprobabilidad, redimpacto from SalvaguardasRiesgos where catsg='"+catsg_wsid+catsg+"' and catrsg='"+catrsg_wsid+catrsg+"' limit 1")
+            .then(function (value) {
+                if (value.length > 0) {
+                    var redprobabilidad = value[0]['redprobabilidad'];
+                    var redimpacto = value[0]['redimpacto'];
 
-        ExecuteFunctions('getModuleWebseriviceID','wsmodule=CatalogoSalvaGuarda').then(function(data) {
-            catsg_wsid = data;
-
-            ExecuteFunctions('getModuleWebseriviceID','wsmodule=CatalogoRiesgos').then(function(data) {
-                catrsg_wsid = data;
-
-                cbws.doQuery("select redprobabilidad, redimpacto from SalvaguardasRiesgos where catsg='"+catsg_wsid+'x'+catsg+"' and catrsg='"+catrsg_wsid+'x'+catrsg+"' limit 1")
-                    .then(function (value) {
-                        if (value.length > 0) {
-                            var redprobabilidad = value[0]['redprobabilidad'];
-                            var redimpacto = value[0]['redimpacto'];
-
-                            if (document.EditView) {
-                                if (document.EditView.redprobabilidad) {
-                                    document.EditView.redprobabilidad.value = redprobabilidad;
-                                }
-                        
-                                if (document.EditView.redimpacto) {
-                                    document.EditView.redimpacto.value = redimpacto;
-                                }
-                            }
+                    if (document.EditView) {
+                        if (document.EditView.redprobabilidad) {
+                            document.EditView.redprobabilidad.value = redprobabilidad;
                         }
-                    });
+                
+                        if (document.EditView.redimpacto) {
+                            document.EditView.redimpacto.value = redimpacto;
+                        }
+                    }
+                }
             });
-        });
     }
 }
